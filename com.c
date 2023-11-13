83eabbd21a6e53e63e9aafc037fd53c6f5c380d4
effaroukk <efqay1@gmail.com>
2023-11-13 19:57:40 +0000

    updated

--- com.c
+++ com.c
@@ -1,69 +1,73 @@
 #include "fk.h"
 
-void null_command(FILE *stream, int count) {
-    fprintf(stream, "Error: Null or empty command in execution attempt #%d\n", count);
-    /* Customize the error message or take other actions as needed*/
+void null_command(FILE *stream, int count)
+{
+	fprintf(stream, "Error: Null or empty
+			command in execution attempt #%d\n", count);
 }
 
 
 
-void env_end(char **env) {
-         int i = 0;
-    while (env[i] != NULL) {
-        printf("%s\n", env[i]);
-        i++;
-    }
+void env_end(char **env)
+{
+	int i = 0;
+
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
+void _path(char **commands, char **env);
+void _path(char **commands, char **env)
+{
+	char *token;
+	char *path_copy;
+	char *path = getenv("PATH");
 
-void _path(char **commands, char **env) {
-	 char *token;
-	 char *path_copy;
-    
-         char *path = getenv("PATH");
-    if (path == NULL) {
-        fprintf(stderr, "Error: PATH environment variable not set\n");
-        return;
-    }
+	if (path == NULL)
+	{
+		fprintf(stderr, "Error: PATH environment variable not set\n");
+		return;
+	}
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+		perror("strdup");
+		return;
+	}
+	token = strtok(path_copy, ":");
+	while (token != NULL)
+	{
+		/* Build the full path to the executable by
+		  concatenating the directory and the command*/
+		char *full_path = malloc(strlen
+				(token) + 1 + strlen(commands[0]) + 1);
 
-  
-   
- path_copy = strdup(path);
-    if (path_copy == NULL) {
-        perror("strdup");
-        return;
-    }
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			free(path_copy);
+			return;
+		}
 
-    token = strtok(path_copy, ":");
-    while (token != NULL) {
-        /* Build the full path to the executable by concatenating the directory and the command*/
-        char *full_path = malloc(strlen(token) + 1 + strlen(commands[0]) + 1);
-        if (full_path == NULL) {
-            perror("malloc");
-            free(path_copy);
-            return;
-        }
-
-        strcpy(full_path, token);
-        strcat(full_path, "/");
-        strcat(full_path, commands[0]);
-
-        /* Check if the executable exists in the current directory*/
-        if (access(full_path, X_OK) == 0) {
-            execve(full_path, commands, env);
-            perror("execve");
-            free(full_path);
-            free(path_copy);
-            return;
-        }
-
-        free(full_path);
-
-        /* Move to the next directory in $PATH*/
-        token = strtok(NULL, ":");
-    }
-
-    /* If we reach here, the executable was not found in any $PATH directory*/
-    fprintf(stderr, "Error: Command not found in $PATH\n");
-
-    free(path_copy);
-}
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, commands[0]);
+		/* Check if the executable exists in the current directory*/
+		if (access(full_path, X_OK) == 0)
+		{
+			execve(full_path, commands, env);
+			perror("execve");
+			free(full_path);
+			free(path_copy);
+			return;
+		}
+		free(full_path);
+		/* Move to the next directory in $PATH*/
+		token = strtok(NULL, ":");
+	}
+	/* If we reach here, the executable was not found in any $PATH directory*/
+	fprintf(stderr, "Error: Command not found in $PATH\n");
+	free(path_copy);
+`}
